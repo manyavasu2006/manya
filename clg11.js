63d3af4d7e31b52e0c80faa41a588e5a5881d33d
manyavasu2006 <manyavasu2006@gmail.com>
2026-08-07 12:50:07 +0530

    Add files via upload

---
+++ clg11.js
@@ -0,0 +1,8 @@
+let a=10;
+let b=20;
+console.log(a==b);
+console.log(a!=b);
+console.log(b>a);
+console.log(b<a);
+console.log(a>=b);
+console.log(a<=b);
---
+++ clg13.js
@@ -0,0 +1,37 @@
+function add(a,b)
+{
+    console.log(a+b);
+}
+add(10,20);
+add(5,15);
+
+function sub(a,b)
+{
+    console.log(a-b);
+}
+sub(30,20);
+sub(5,15);
+
+function mul(a,b)
+{
+    console.log(a*b);
+}
+mul(10,20);
+mul(5,15);
+
+function div(a,b)
+{
+    console.log(a/b);
+}
+div(10,20);
+div(5,15);
+
+function opr(a,b)
+{
+    console.log(a+b);
+    console.log(a-b);
+    console.log(a*b);
+    console.log(a/b);
+}
+opr(10,20);
+opr(5,15);
